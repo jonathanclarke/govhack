e986a2657c6299c32b482608b3792a458e92563f
Jonathan Clarke <jonathan@beilabs.com>
2013-06-01 18:16:28 +1000

    Adding twitter-glyphicons, updating locale information + lots more styling data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -23,8 +23,8 @@ class PagesController < ApplicationController
     json_data = File.read("#{ Rails.root }/db/data/universities/#{ params[:university_permalink] }.json")
 
     @university = JSON.parse(json_data)
-    json_data = File.read("#{ Rails.root }/db/data/suburbs/#{ params[:suburb_permalink] }.json")
+    suburb_data = File.read("#{ Rails.root }/db/data/suburbs/#{ params[:suburb_permalink] }.json")
 
-    @university = JSON.parse(json_data)
+    @suburb = JSON.parse(suburb_data)
   end
 end
