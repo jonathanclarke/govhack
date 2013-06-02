b32dc6d93d241f5f755462f6fcd216f2abbced06
Jonathan Clarke <jonathan@beilabs.com>
2013-06-02 12:49:31 +1000

    Adding frontend assets + vendor assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Govhack::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.assets.precompile += ['frontend/*',
+                               'vendor/*']
 end
